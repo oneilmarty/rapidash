6ae330da63e856378a1859370b4135ac4be7e125
Marty O'Neil <oneilmarty@gmail.com>
2017-01-04 11:17:43 -0600

    Added a header and footer to website

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width">
+		<title><?php bloginfo('name'); ?></title>
+		<?php wp_head(); ?>
+	</head>
+<body <?php body_class(); ?>>
+	<!-- site-header -->
+	<header class="site-header">
+		<h1><?php bloginfo('name'); ?></h1>
+		<h5><?php bloginfo('description'); ?></h5>
+	</header>
+	<!-- site-header -->
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+get_header();
+
 if (have_posts()) :
 	while (have_posts()) : the_post(); ?>
 
@@ -12,5 +14,7 @@ if (have_posts()) :
 		echo '<p>No content found</p>';
 
 	endif;
+
+get_footer();
 
 ?>
